c4f729542044fbfcd67bc652cef84a0a7652d765
Karuppusamy M <135962622+karuppusamy1@users.noreply.github.com>
2023-12-08 17:34:46 +0530

    Delete Movies directory

--- Movies/movies/templatetags/numbers.py
+++
@@ -1,9 +0,0 @@
-#movies/templatetags/numbers.py
-from django import template
-
-register = template.Library()
-
-def format_number(value):
-    return f"{value:,}"
-
-register.filter('format_number', format_number)
