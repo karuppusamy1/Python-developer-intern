5c6c8bd9269d7dadb3f20784f71f0403e24ede08
karuppusamy1 <karuppusamykmk1@gmail.com>
2023-12-08 19:06:46 +0530

    Organize into Single Folder

---
+++ chatapp/urls.py
@@ -0,0 +1,13 @@
+#chatapp/urls.py
+from django.urls import path
+from . import views
+
+
+app_name = "chatapp"
+
+urlpatterns = [
+    path('favicon.ico',views.favicon,name='favicon'),
+    path("getchatresponse/", views.getchatresponse, name="getchatresponse"),
+    path('home/',views.home,name='home'),
+    path('index/',views.index,name='index')
+]
