b1e47d8e8fe2b74dd2065db4ff343d3abd25a140
Karuppusamy M <135962622+karuppusamy1@users.noreply.github.com>
2023-12-09 09:18:29 +0530

    Update urls.py

--- Chatbot/chatapp/urls.py
+++ Chatbot/chatapp/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #chatapp/urls.py
 from django.urls import path
 from . import views
@@ -11,18 +11,4 @@ urlpatterns = [
     path("getchatresponse/", views.getchatresponse, name="getchatresponse"),
     path('home/',views.home,name='home'),
     path('index/',views.index,name='index')
-=======
-#chatapp/urls.py
-from django.urls import path
-from . import views
 
-
-app_name = "chatapp"
-
-urlpatterns = [
-    path('favicon.ico',views.favicon,name='favicon'),
-    path("getchatresponse/", views.getchatresponse, name="getchatresponse"),
-    path('home/',views.home,name='home'),
-    path('index/',views.index,name='index')
->>>>>>> c757b4711a9f228aefeee01ee108df6d6dddfde9
-]
