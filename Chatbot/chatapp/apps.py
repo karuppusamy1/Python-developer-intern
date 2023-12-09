8276df578aee964ba3f3712aa0ffb6483525223d
Karuppusamy M <135962622+karuppusamy1@users.noreply.github.com>
2023-12-09 09:26:22 +0530

    Update apps.py

--- Chatbot/chatapp/apps.py
+++ Chatbot/chatapp/apps.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 from django.apps import AppConfig
 
 
 class ChatappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chatapp'
-=======
-from django.apps import AppConfig
-
-
-class ChatappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatapp'
->>>>>>> c757b4711a9f228aefeee01ee108df6d6dddfde9
