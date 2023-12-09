66be489ea789630fe17e3f7849c963508d65cff7
Karuppusamy M <135962622+karuppusamy1@users.noreply.github.com>
2023-12-09 09:20:02 +0530

    Update tests.py

--- Chatbot/chatapp/tests.py
+++ Chatbot/chatapp/tests.py
@@ -1,9 +1,3 @@
 <<<<<<< HEAD
 from django.test import TestCase
 
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> c757b4711a9f228aefeee01ee108df6d6dddfde9
