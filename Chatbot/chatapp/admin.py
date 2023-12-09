56bcba70c8ba31e8cae522a8a95b1cac46e674e0
Karuppusamy M <135962622+karuppusamy1@users.noreply.github.com>
2023-12-09 09:16:39 +0530

    Update admin.py

--- Chatbot/chatapp/admin.py
+++ Chatbot/chatapp/admin.py
@@ -3,7 +3,3 @@ from django.contrib import admin
 
 # Register your models here.
 =======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> c757b4711a9f228aefeee01ee108df6d6dddfde9
